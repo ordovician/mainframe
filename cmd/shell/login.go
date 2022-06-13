7d41450ed7dc8ead01da250ba33847c9283cb73d
Erik Engheim <ordovician.cenozoic@mac.com>
2022-06-13 23:35:35 +0200

    Factor out Login function
    This allows us to use different reader and writer for stdout and stdin

--- cmd/shell/login.go
+++ cmd/shell/login.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	. "github.com/ordovician/mainframe"
+	"io"
 	"log"
 	"os"
 	"time"
@@ -11,33 +12,51 @@ import (
 
 var debug *log.Logger = log.New(os.Stdout, "DEBUG: ", 0)
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("Login: ")
+type Terminal struct {
+	Stdin  io.Reader
+	Stdout io.Writer
+	Stderr io.Writer
+}
+
+func (term *Terminal) Login() {
+	scanner := bufio.NewScanner(term.Stdin)
+	fmt.Fprint(term.Stdout, "Login: ")
 	if !scanner.Scan() {
-		fmt.Fprintln(os.Stderr, "Unable to get login name:", scanner.Err())
+		fmt.Fprintln(term.Stderr, "Unable to get login name:", scanner.Err())
 		os.Exit(1)
 	}
 
 	user := scanner.Text()
 
-	fmt.Print("Password: ")
+	fmt.Fprint(term.Stdout, "Password: ")
 	if !scanner.Scan() {
-		fmt.Fprintln(os.Stderr, "Unable to get password:", scanner.Err())
+		fmt.Fprintln(term.Stderr, "Unable to get password:", scanner.Err())
 		os.Exit(1)
 	}
 
 	passwd := scanner.Text()
 
 	if ok, err := CheckLogin(user, passwd); ok {
+		curtime := time.Now().Local().Format("January 2, 2006")
+
 		// Name from Soviet BESM-6 computer https://en.wikipedia.org/wiki/BESM
-		fmt.Printf("Большая Электронно-Счётная Машина 6: %v\n", time.Now().Local().Format("January 2, 2006"))
-		fmt.Printf("Welcome to БЭСМ-6 comrade %s\n", user)
+		fmt.Fprintf(term.Stdout, "Большая Электронно-Счётная Машина 6: %v\n", curtime)
+		fmt.Fprintf(term.Stdout, "Welcome to БЭСМ-6 comrade %s\n", user)
 
 		runShell()
 	} else if err != nil {
-		fmt.Fprintln(os.Stderr, "Could not log in becase:", err)
+		fmt.Fprintln(term.Stderr, "Could not log in because:", err)
 	} else {
-		fmt.Print("Username does not exist or password was wrong")
+		fmt.Fprint(term.Stdout, "Username does not exist or password was wrong")
 	}
+}
+
+func main() {
+	term := &Terminal{
+		os.Stdin,
+		os.Stdout,
+		os.Stderr,
+	}
+
+	term.Login()
 }
