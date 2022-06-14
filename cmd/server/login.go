18c1dfc76b49e79ce4d1c06acf1fa5c471d6a998
Erik Engheim <erik.engheim@mac.com>
2022-06-14 21:21:23 +0200

    Allow configuring port number and protocol

--- cmd/server/login.go
+++ cmd/server/login.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
 	"log"
 	"net"
 	"os"
+	"strings"
 
 	. "github.com/ordovician/mainframe"
 )
@@ -12,9 +15,29 @@ import (
 var debug *log.Logger = log.New(os.Stdout, "DEBUG: ", 0)
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stdout, "Usage of server:\n")
+		flag.PrintDefaults()
+	}
+
+	var (
+		port     int
+		protocol string
+	)
+
+	flag.IntVar(&port, "port", 1234, "Port number for server to listen for connections on")
+	flag.StringVar(&protocol, "protocol", "tcp", "Protocol such as TCP or UDP for socket connection")
+	flag.Parse()
+
+	protocol = strings.ToLower(protocol)
+	portStr := fmt.Sprintf(":%d", port)
+
+	fmt.Printf("Starting server on port %d using protocol %s\n", port, protocol)
+	fmt.Println("Hit Ctrl-C to stop")
+
 	// listen on port 1234
 	// you can connect using NetCat or telnet, see README.md
-	server, err := net.Listen("tcp", ":1234")
+	server, err := net.Listen(protocol, portStr)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
